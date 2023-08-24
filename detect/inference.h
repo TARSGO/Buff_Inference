40e6d45baac749b68912281dc49b95746742bfc7
zhbzhb <431433788@qq.com>
2023-08-24 12:29:09 +0800

    测试可用

--- detect/inference.h
+++ detect/inference.h
@@ -1,72 +1,44 @@
 //
-// Created by shaobing2 on 8/20/23.
+// Created by shaobing2 on 8/24/23.
 //
 
 #ifndef WIND_INFERENCE_INFERENCE_H
 #define WIND_INFERENCE_INFERENCE_H
 
-#include <iterator>
-#include <memory>
-#include <string>
-#include <vector>
-#include <iostream>
-
-#include <inference_engine.hpp>
+#include <opencv2/dnn.hpp>
+#include <openvino/openvino.hpp>
 #include <opencv2/opencv.hpp>
-#include <Eigen/Core>
-
-
 using namespace std;
-using namespace cv;
-using namespace InferenceEngine;
 
-struct alignas(4) bbox_t {
-    cv::Point2f pts[5]; // [pt0, pt1, pt2, pt3]
-    float confidence;
-    int color_id; // 0: blue, 1: red, 2: gray
-    int tag_id;   // 0: guard, 1-5: number, 6: base
-
-    bool operator==(const bbox_t&) const = default;
-    bool operator!=(const bbox_t&) const = default;
-};
-
-struct BuffObject
+struct Detection
 {
-    Point2f apex[5];
-    cv::Rect_<float> rect;
     int cls;
     int color;
-    float prob;
-    std::vector<cv::Point2f> pts;
+    float confidence;
+    cv::Point2f ppts[5];
+    cv::Rect rect;
+};
+
+struct Resize
+{
+    cv::Mat resized_image;
+    int dw;
+    int dh;
 };
 
 class BuffDetector
 {
-
-private:
-    Core ie;
-    CNNNetwork network;                // 网络
-    ExecutableNetwork executable_network;       // 可执行网络
-    InferRequest infer_request;      // 推理请求
-    MemoryBlob::CPtr moutput;
-    string input_name;
-    string output_name;
-
-    Eigen::Matrix<float,3,3> transfrom_matrix;
-
 public:
-    BuffDetector();
-    ~BuffDetector();
 
-    bool detect(Mat &src,vector<BuffObject>& objects);
+    bool detect(cv::Mat &src,vector<Detection>& output);
     bool initModel(string path);
 
+private:
+    ov::Core core;
+    std::shared_ptr<ov::Model> model;
+    ov::CompiledModel compiled_model;
+    ov::InferRequest infer_request;
 };
-struct GridAndStride
-{
-    int grid0;
-    int grid1;
-    int stride;
-};
+
 
 #endif //WIND_INFERENCE_INFERENCE_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "detect/inference.h"
+
+int main(){
+    BuffDetector buff;
+    buff.initModel("../model/buff2.xml");
+    // Step 3. Read input image
+    cv::VideoCapture cap;
+    cap.open( "../test.mp4");
+    cv::Mat img;
+
+    while(1)
+    {
+        cap>>img;
+        std::vector<Detection> output;
+        buff.detect(img,output);
+        cv::waitKey(1);
+    }
+
 }
+
+
+
+
+
+
+
+//#include "detect/inference.h"
+//int main() {
+//    cv::VideoCapture cap;
+//    cap.open( "../test.mp4");
+//    cv::Mat frame;
+//    while(1)
+//    {
+//        cap >> frame;
+//        vector<BuffObject> objects;
+//        vector<Mat> fans;
+//        auto input = frame;
+//        string name[6] = {"BR","BU","BA","RR","RU","RA"};
+//
+//        BuffDetector b;
+//        b.initModel("../model/buff2.xml");
+//        b.detect(input,objects);
+//        for(auto object: objects)
+//        {
+//            auto cl = object.color*3 + object.cls;
+//            string n = name[cl];
+//            line(input, object.apex[0], object.apex[1], Scalar(0, 255, 0), 2);
+//            line(input, object.apex[1], object.apex[2], Scalar(0, 255, 0), 2);
+//            line(input, object.apex[2], object.apex[3], Scalar(0, 255, 0), 2);
+//            line(input, object.apex[3], object.apex[4], Scalar(0, 255, 0), 2);
+//            line(input, object.apex[4], object.apex[0], Scalar(0, 255, 0), 2);
+//            putText(input, n, object.apex[0], 1, 1, Scalar(0, 255, 0));
+//        }
+//        namedWindow("network_input",0);
+//        imshow("network_input",input);
+//        waitKey(1);
+//    }
+//
+//    return 0;
+//}
